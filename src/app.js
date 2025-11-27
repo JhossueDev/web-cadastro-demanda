a6c485ddc5a91c9cfc8929862fcc0212404ae2ee
Jhossue <jhossue.dev@gmail.com>
2025-11-27 09:57:00 -0300

    Ajuste de front-end e caminhos de CSS/JS
    Corrigi os caminhos do CSS/JS pra carregar certinho nas páginas
    Ajustei as rotas pra servir os HTMLs da pasta views
    Testei todas as páginas e tudo tá funcionando via localhost

--- src/app.js
+++ src/app.js
@@ -3,8 +3,30 @@ import cors from "cors";
 import produtoRotas from "../routes/produtoRotas.js";
 import vendaRotas from "../routes/vendaRotas.js";
 import previsaoRotas from "../routes/previsaoRotas.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
+//rota principal
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(_dirname, "../views/index.html"));
+});
+
+app.get("/cadastro", (req, res) =>{
+    res.sendFile(path.join(_dirname, "../views/cadastro.html"));
+});
+
+app.get("/vendas", (req, res) =>{
+    res.sendFile(path.join(_dirname, "../views/vendas.html"));
+});
+
+app.get("/projecao", (req, res) =>{
+    res.sendFile(path.join(_dirname, "../views/projecao.html"))
+});
 
 app.use(express.json());
 app.use(cors());
